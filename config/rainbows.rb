008346b48bec2ab131fdb2986b32bbc28460e06f
wildjcrt <wildjcrt@yahoo.com.tw>
2015-12-02 12:28:45 +0800

    Adjust config/rainbows.rb to setup stderr_path and stdout_path

--- config/rainbows.rb
+++ config/rainbows.rb
@@ -1,4 +1,15 @@
-worker_processes 10
+app_path = File.join(File.dirname(__FILE__), '..')
+working_directory app_path
+
+if ENV["RAILS_ENV"] == "production"
+	pid "#{app_path}/tmp/pids/rainbows.pid"
+	stderr_path "#{app_path}/log/rainbows.log"
+	stdout_path "#{app_path}/log/rainbows.log"
+	worker_processes 5
+else
+	worker_processes 1
+end
+preload_app true
 timeout 30
 
 Rainbows! do
