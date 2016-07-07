69f2083a98b50e8b62d9be679dbdd10aa5734898
wildjcrt <wildjcrt@yahoo.com.tw>
2016-07-07 10:50:12 +0800

    Add 201607skl

--- app.rb
+++ app.rb
@@ -52,7 +52,7 @@ class Say
     image = MiniMagick::Image.open("public/images/#{folder}/#{filename}.jpg")
 
     case folder
-    when "vday2015", "201511sofina", "2016changedestiny", "2016YoungFuture"
+    when "vday2015", "201511sofina", "2016changedestiny", "2016YoungFuture", "201607skl"
       position = 60
       quote_function1 = "text 0,-#{position} '#{quote1}'"
       quote_function2 = "text 0,0 '#{quote2}'"
