4abae6ada707e1df1b770f2f1009859c36765a43
wildjcrt <wildjcrt@yahoo.com.tw>
2016-11-24 11:56:33 +0800

    Fix 2016goldenhourse

--- app.rb
+++ app.rb
@@ -54,7 +54,7 @@ class Say
     case folder
     when "vday2015", "201511sofina",
          "2016changedestiny", "2016YoungFuture", "2016lemuria",
-         "201607skl", "201607BenQ", "201610samsung", 'goldenhourse2016'
+         "201607skl", "201607BenQ", "201610samsung", '2016goldenhourse'
       position = 60
       quote_function1 = "text 0,-#{position} '#{quote1}'"
       quote_function2 = "text 0,0 '#{quote2}'"
